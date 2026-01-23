47c286603cd0aa3c9fb4cc8369c9e0733b698173
kmlitchen <litchen@wisc.edu>
2026-01-22 18:55:08 -0600

    Add files via upload

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,8 @@
 // Created by: Katie Litchen
 
 var mydiv = document.getElementById("world");
-    mydiv.addEventListener("click",function()(
-        alert("Hello World!");
-    ));
+mydiv.addEventListener("click",function(){
+    alert("Hello World!");
+});
+
+console.log("Hello World!")
